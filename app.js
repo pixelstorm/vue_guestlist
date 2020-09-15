c65a223b3961e8908e1bd0bb057f1880acca6c8c
pixelstorm <dan@pixelstorm.com.au>
2020-09-15 19:22:03 +1000

    setup the vue event data object to make the app more scalable

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 new Vue({
 
 	el: '#app', 
+
 	data: {
-		eventDate: 'August 14 2000',
-		eventTitle: 'Summer Festival',
-		eventDescription: 'a very fun event'
+
+		event: {
+			eventDate: 'August 14 2000',
+			eventTitle: 'Summer Festival',
+			eventDescription: 'a very fun event'
+		}
 	}
 
 
